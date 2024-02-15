9724ffa44ec9dff2095c2de711977a221e3ba12b
BrunoTI-Code <155826835+BrunoTI-Code@users.noreply.github.com>
2024-02-14 22:13:56 -0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+// Home.tsx
+import React from 'react';
+import { Menu } from '../componentes/Menu';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Menu />
+      <h1>Página inicial!</h1>
+    </div>
+  );
+};
+
+
+
+export default Home;
+
+
---
+++ classes/controle/ControleEditora.ts
@@ -0,0 +1,25 @@
+import { Editora } from '../modelo/Editora';
+
+// Array de objetos Editora que representa as editoras existentes
+
+let editoras: Editora[] = [
+  new Editora(1, 'Intrínseca'),
+  new Editora(2, 'Globo'),
+  new Editora(3, 'Galera'),
+];
+
+export class ControleEditora {
+  
+  //obter o nome da editora com base no código da editora
+  
+  getNomeEditora(codEditora: number) {
+    const editora = editoras.find(e => e.codEditora === codEditora);
+    return editora ? editora.nome : '';
+  }
+
+  //obter a lista completa de editoras
+  
+  getEditoras() {
+    return editoras;
+  }
+}
